6a3ae2ecca570d3904a64f3f15e0446b9e03ab96
krokosik <wikkra@gmail.com>
2024-12-17 00:56:59 +0100

    Day 15 solution

--- src/day15.rs
+++ src/day15.rs
@@ -1,4 +1,8 @@
-use std::{fmt, ops::{Add, AddAssign, Index, IndexMut}};
+use std::{
+    collections::VecDeque,
+    fmt,
+    ops::{Add, AddAssign, Index, IndexMut, Neg},
+};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 enum Direction {
@@ -16,6 +20,19 @@ impl From<char> for Direction {
             '<' => Direction::Left,
             '>' => Direction::Right,
             _ => panic!("Invalid direction"),
+        }
+    }
+}
+
+impl Neg for Direction {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
         }
     }
 }
@@ -215,6 +232,237 @@ fn part1(input: &str) -> u64 {
     wh.get_box_coordinates()
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+enum ScaledField {
+    Wall,
+    BoxLeft,
+    BoxRight,
+    Robot,
+    Clear,
+}
+
+impl<'a> From<&'a ScaledField> for char {
+    fn from(value: &'a ScaledField) -> Self {
+        match value {
+            ScaledField::Wall => '#',
+            ScaledField::BoxLeft => '[',
+            ScaledField::BoxRight => ']',
+            ScaledField::Robot => '@',
+            ScaledField::Clear => '.',
+        }
+    }
+}
+
+struct ScaledWarehouse {
+    fields: Vec<Vec<ScaledField>>,
+    robot_pos: Pos,
+    width: usize,
+}
+
+impl From<Warehouse> for ScaledWarehouse {
+    fn from(wh: Warehouse) -> Self {
+        let mut fields = Vec::with_capacity(wh.height);
+        let mut robot_pos = wh.robot_pos;
+
+        for (y, row) in wh.fields.iter().enumerate() {
+            fields.push(Vec::with_capacity(wh.width * 2));
+
+            for (x, field) in row.iter().enumerate() {
+                fields[y].push(match field {
+                    Field::Wall => ScaledField::Wall,
+                    Field::Box => ScaledField::BoxLeft,
+                    Field::Clear => ScaledField::Clear,
+                    Field::Robot => {
+                        robot_pos = Pos {
+                            x: x as isize * 2,
+                            y: y as isize,
+                        };
+                        ScaledField::Robot
+                    }
+                });
+                fields[y].push(match field {
+                    Field::Wall => ScaledField::Wall,
+                    Field::Box => ScaledField::BoxRight,
+                    Field::Clear | Field::Robot => ScaledField::Clear,
+                });
+            }
+        }
+
+        ScaledWarehouse {
+            fields,
+            robot_pos,
+            width: wh.width * 2,
+        }
+    }
+}
+
+impl<'a> Index<&'a Pos> for ScaledWarehouse {
+    type Output = ScaledField;
+
+    fn index(&self, pos: &'a Pos) -> &Self::Output {
+        &self.fields[pos.y as usize][pos.x as usize]
+    }
+}
+
+impl<'a> IndexMut<&'a Pos> for ScaledWarehouse {
+    fn index_mut(&mut self, pos: &'a Pos) -> &mut Self::Output {
+        &mut self.fields[pos.y as usize][pos.x as usize]
+    }
+}
+
+impl fmt::Display for ScaledWarehouse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.fields {
+            let s: String = row.iter().map(char::from).collect();
+            writeln!(f, "{}", s)?;
+        }
+        Ok(())
+    }
+}
+
+impl ScaledWarehouse {
+    fn move_robot(&mut self, dir: &Direction) {
+        if self.move_boxes(dir) {
+            let robot_pos = self.robot_pos.clone();
+            self[&robot_pos] = ScaledField::Clear;
+            self.robot_pos += dir;
+            self[&(robot_pos + dir)] = ScaledField::Robot;
+        }
+    }
+
+    fn move_boxes(&mut self, dir: &Direction) -> bool {
+        let mut new_pos = self.robot_pos + dir;
+        let inv_dir = &(-(*dir));
+
+        if self[&new_pos] == ScaledField::Clear {
+            return true;
+        }
+
+        match dir {
+            Direction::Up | Direction::Down => {
+                let mut stack = VecDeque::new();
+                stack.push_back(new_pos);
+                let mut visited = vec![new_pos];
+
+                while let Some(pos) = stack.pop_front() {
+                    match self[&pos] {
+                        ScaledField::Wall => return false,
+                        ScaledField::BoxLeft => {
+                            for new_dir in &[&Direction::Right, dir] {
+                                let new_pos = pos + new_dir;
+                                if !visited.contains(&new_pos) {
+                                    stack.push_back(new_pos);
+                                    visited.push(new_pos);
+                                }
+                            }
+                            let new_pos = pos + dir + &Direction::Right;
+                            if !visited.contains(&new_pos) {
+                                stack.push_back(new_pos);
+                                visited.push(new_pos);
+                            }
+                        }
+                        ScaledField::BoxRight => {
+                            for new_dir in &[&Direction::Left, dir] {
+                                let new_pos = pos + new_dir;
+                                if !visited.contains(&new_pos) {
+                                    stack.push_back(new_pos);
+                                    visited.push(new_pos);
+                                }
+                            }
+                            let new_pos = pos + dir + &Direction::Left;
+                            if !visited.contains(&new_pos) {
+                                stack.push_back(new_pos);
+                                visited.push(new_pos);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+
+                while visited.len() > 2 {
+                    let pos = visited.pop().unwrap();
+                    if !visited.contains(&(pos + inv_dir)) {
+                        continue;
+                    }
+                    let field = self[&pos];
+                    self[&pos] = self[&(pos + inv_dir)];
+                    self[&(pos + inv_dir)] = field;
+                }
+
+                while let Some(first_box_pos) = visited.pop() {
+                    self[&first_box_pos] = ScaledField::Clear;
+                }
+
+                true
+            }
+            Direction::Left | Direction::Right => {
+                loop {
+                    match self[&new_pos] {
+                        ScaledField::Clear => break,
+                        ScaledField::Wall => return false,
+                        _ => new_pos += dir,
+                    }
+                }
+                while new_pos != self.robot_pos + dir {
+                    let field = self[&new_pos];
+                    self[&new_pos] = self[&(new_pos + inv_dir)];
+                    self[&(new_pos + inv_dir)] = field;
+                    new_pos += inv_dir;
+                }
+                true
+            }
+        }
+    }
+
+    fn get_box_coordinates(&self) -> u64 {
+        self.fields
+            .iter()
+            .flatten()
+            .enumerate()
+            .filter(|(_, field)| match field {
+                ScaledField::BoxLeft => true,
+                _ => false,
+            })
+            .map(|(i, _)| (i / self.width * 100 + i % self.width) as u64)
+            .sum()
+    }
+
+    #[allow(dead_code)]
+    fn detect_broken_boxes(&self) -> bool {
+        self.fields
+            .iter()
+            .flatten()
+            .enumerate()
+            .filter(|(_, field)| match field {
+                ScaledField::BoxLeft => true,
+                _ => false,
+            })
+            .any(|(i, _)| {
+                let x = i % self.width;
+                let y = i / self.width;
+                let right = self.fields[y][x + 1];
+
+                match right {
+                    ScaledField::BoxRight => false,
+                    _ => true,
+                }
+            })
+    }
+}
+
+#[aoc(day15, part2)]
+fn part2(input: &str) -> u64 {
+    let mut lines = input.lines();
+    let wh: Warehouse = lines.by_ref().take_while(|l| !l.is_empty()).collect();
+    let mut swh: ScaledWarehouse = ScaledWarehouse::from(wh);
+
+    lines.flat_map(|l| l.chars()).for_each(|c| {
+        swh.move_robot(&c.into());
+    });
+
+    swh.get_box_coordinates()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -233,5 +481,53 @@ mod tests {
     #[test]
     fn simple_example() {
         assert_eq!(part1(SIMPLE_INPUT), 2028);
+    }
+
+    #[test]
+    fn partial_wall_block() {
+        assert_eq!(
+            part2(
+                "#######
+#.....#
+#.O.O@#
+#..O..#
+#..O..#
+#.....#
+#######
+
+<v<<>vv<^^"
+            ),
+            822
+        )
+    }
+
+    #[test]
+    fn large_example2() {
+        assert_eq!(
+            part2(
+                "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+            ),
+            9021
+        );
     }
 }
